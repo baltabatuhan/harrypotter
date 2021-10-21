a6c6d57b057134aab1cc2be6ada3b3b807c2a168
Batuhan Balta <bthn.balta@gmail.com>
2021-10-21 03:24:06 -0700

    useeffect

--- src/App.js
+++ src/App.js
@@ -1,35 +1,45 @@
-
 import { useEffect, useState } from "react";
-import './App.css';
+import "./App.css";
 import axios from "axios";
 
-
-
-
-
-
-
-
 function App() {
-  const [quote, setQuote] = useState()
-  const [character, setCharacter] = useState();
-
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
 
     axios
-      .get('http://hp-api.herokuapp.com/api/characters')
+      .get("http://hp-api.herokuapp.com/api/characters")
       .then((response) =>
-        console.log(response.data, "zaza")
+        setCharacters(
+          response.data.map((character) => ({
+            characters: [],
+            name: character.name,
+            gender: character.gender,
+            house: character.house,
+            birth: character.birth,
+            wand: character.wand,
+            patronus: character.patronus,
+            hogwartsStudent: character.hogwartsStudent,
+            hogwartsStaff: character.hogwartsStaff,
+            ancestry: character.ancestry,
+            species: character.species,
+            image: character.image,
+          }))
+        )
       )
 
       .catch((error) => console.log({ error }));
   }, []);
 
+  if (characters.length === 0) {
+    return <div>loading...</div>;
+  }
+  console.log(characters.species, "zaza");
+
 
   return (
     <div className="App">
-
+      <p>{characters.species}</p>
     </div>
   );
 }
